162a29f87536acdc9921af270a9460671978b2af
Danish Mehmood <42686972+danish981@users.noreply.github.com>
2020-12-22 06:20:15 +0500

    Add files via upload

---
+++ src/DS/Arrays.java
@@ -0,0 +1,7 @@
+package DS;
+
+public class Arrays {
+
+
+
+}
---
+++ src/PractPack/ArraysTest.java
@@ -0,0 +1,13 @@
+package PractPack;
+
+/**
+ * Created by Developers on 17-Dec-20.
+ */
+public class ArraysTest {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
